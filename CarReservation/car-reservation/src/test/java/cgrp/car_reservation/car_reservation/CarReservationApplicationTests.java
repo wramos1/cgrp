708f28793a328742bcdb180ab9244b67c8128f34
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2024-09-23 14:02:03 -0700

    Initial commit

---
+++ CarReservation/car-reservation/src/test/java/cgrp/car_reservation/car_reservation/CarReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package cgrp.car_reservation.car_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
