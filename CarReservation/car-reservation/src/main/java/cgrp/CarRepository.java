b3554e7103767723c128c718e2a4780b4e490751
Will Ramos <ramoswillyam1@gmail.com>
2024-10-04 16:38:13 -0700

    Testing API endpoints and making sure it is receiving responses

---
+++ CarReservation/car-reservation/src/main/java/cgrp/CarRepository.java
@@ -0,0 +1,10 @@
+package cgrp;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends MongoRepository<Car, ObjectId> {
+
+}
