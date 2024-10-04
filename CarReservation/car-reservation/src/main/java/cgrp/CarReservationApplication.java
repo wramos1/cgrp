b95efda972c39b4d8f9b321d00e5d2f4e3c875bb
Will Ramos <ramoswillyam1@gmail.com>
2024-10-04 16:36:21 -0700

    Removed System printing a name line

---
+++ CarReservation/car-reservation/src/main/java/cgrp/CarReservationApplication.java
@@ -0,0 +1,13 @@
+package cgrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarReservationApplication.class, args);
+	}
+
+}
