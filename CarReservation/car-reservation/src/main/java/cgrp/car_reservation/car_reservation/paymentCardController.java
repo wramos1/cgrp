c4f9d02e56936098589b614c7d12bb913b22279b
Arthur Lazaryan <artlazaryan86@gmail.com>
2024-10-07 15:12:46 -0700

    Minor additions made

--- CarReservation/car-reservation/src/main/java/cgrp/car_reservation/car_reservation/paymentCardController.java
+++ CarReservation/car-reservation/src/main/java/cgrp/car_reservation/car_reservation/paymentCardController.java
@@ -1,6 +1,7 @@
 package cgrp.car_reservation.car_reservation;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cards")
 public class paymentCardController {
 
-    @PostMapping("/newcard")
-    public
+   @PostMapping("/newcard")
+    public paymentCard createCard(@RequestBody paymentCard card)
+   {
 
+       return card;
+   }
 }
--- CarReservation/car-reservation/src/main/java/cgrp/car_reservation/car_reservation/paymentCardService.java
+++ CarReservation/car-reservation/src/main/java/cgrp/car_reservation/car_reservation/paymentCardService.java
@@ -1,6 +1,16 @@
 package cgrp.car_reservation.car_reservation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class paymentCardService {
+
+
+    @Autowired
+    private paymentCardRepository cardRepository;
+
+
+
+
 
 
 }
