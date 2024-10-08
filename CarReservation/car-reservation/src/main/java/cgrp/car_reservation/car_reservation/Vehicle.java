03cfd2a531e88556b08cfa215b32372e521d57be
Arthur Lazaryan <artlazaryan86@gmail.com>
2024-10-07 21:18:43 -0700

    Made modifications to Vehicle class

--- CarReservation/car-reservation/src/main/java/cgrp/car_reservation/car_reservation/Vehicle.java
+++ CarReservation/car-reservation/src/main/java/cgrp/car_reservation/car_reservation/Vehicle.java
@@ -6,6 +6,8 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Getter @Document(collection = "vehicles")
 public class Vehicle {
     @Id
@@ -15,29 +17,121 @@ public class Vehicle {
     private int year;
     private String type;
     private String color;
-    private Review[] reviews;
-    private double rating = calculateAverageRating();
-    private int chargePerDay;
-    private @Setter boolean status;
-    private String description;
-    private String[] features;
+    private double averageRating;
+    private int dailyRentRate;
+    private Boolean currentlyRented;
+    private String vehicleDescription;
+    private List<Review> reviewsOfVehicle;
+    private List<String> vehicleFeatures;
 
-    private String carName;  
-
-    public Vehicle(){}
-
-    private double calculateAverageRating() {
-        if (reviews == null || reviews.length == 0) {
-            return 0.0; // Return 0 or some sensible default
-        }
-
-        int sum = 0;
-        for (Review review : reviews) {
-            sum += review.getRating();
-        }
-
-        return (double) sum / reviews.length; // Cast to double for a more accurate average
+    public Vehicle(List<String> vehicleFeatures, List<Review> reviewsOfVehicle, String vehicleDescription, Boolean currentlyRented, double averageRating, int dailyRentRate, String color, String type, int year, String model, String make, ObjectId vehicleID) {
+        this.vehicleFeatures = vehicleFeatures;
+        this.reviewsOfVehicle = reviewsOfVehicle;
+        this.vehicleDescription = vehicleDescription;
+        this.currentlyRented = currentlyRented;
+        this.averageRating = averageRating;
+        this.dailyRentRate = dailyRentRate;
+        this.color = color;
+        this.type = type;
+        this.year = year;
+        this.model = model;
+        this.make = make;
+        this.vehicleID = vehicleID;
     }
 
+    public ObjectId getVehicleID() {
+        return vehicleID;
+    }
 
+    public void setVehicleID(ObjectId vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public List<String> getVehicleFeatures() {
+        return vehicleFeatures;
+    }
+
+    public void setVehicleFeatures(List<String> vehicleFeatures) {
+        this.vehicleFeatures = vehicleFeatures;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(double averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    public int getDailyRentRate() {
+        return dailyRentRate;
+    }
+
+    public void setDailyRentRate(int dailyRentRate) {
+        this.dailyRentRate = dailyRentRate;
+    }
+
+    public Boolean getCurrentlyRented() {
+        return currentlyRented;
+    }
+
+    public void setCurrentlyRented(Boolean currentlyRented) {
+        this.currentlyRented = currentlyRented;
+    }
+
+    public String getVehicleDescription() {
+        return vehicleDescription;
+    }
+
+    public void setVehicleDescription(String vehicleDescription) {
+        this.vehicleDescription = vehicleDescription;
+    }
+
+    public List<Review> getReviewsOfVehicle() {
+        return reviewsOfVehicle;
+    }
+
+    public void setReviewsOfVehicle(List<Review> reviewsOfVehicle) {
+        this.reviewsOfVehicle = reviewsOfVehicle;
+    }
 }
