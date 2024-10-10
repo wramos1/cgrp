ff139c5b210932281136f3dc3d65de6c8ae9a8df
Arthur Lazaryan <artlazaryan86@gmail.com>
2024-10-09 20:58:56 -0700

    Slight modifications made to vehicle class and @Document Reference Annotation for feature class

--- CarReservation/car-reservation/src/main/java/cgrp/car_reservation/car_reservation/Vehicle.java
+++ CarReservation/car-reservation/src/main/java/cgrp/car_reservation/car_reservation/Vehicle.java
@@ -5,25 +5,36 @@ import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-@Getter @Document(collection = "vehicles")
+import java.util.List;
+
+@Setter
+@Getter
+@Document(collection = "vehicles")
 public class Vehicle {
     @Id
     private ObjectId vehicleID;
+
     private String make;
     private String model;
     private int year;
     private String type;
     private String color;
-    private Review[] reviewsOfVehicle;
-    private double rating = calculateAverageRating();
     private int dailyRentRate;
-    private @Setter boolean currentlyRented;
-    private String description;
-    private String[] vehicleFeatures;
+    private double vehicleRating = 0.0; // the average rating of the vehicle ; start it at 0.0 b/c it currently has no ratings
+    private boolean currentlyRented; // boolean value if it is currently rented or not
+    private String description; // quick descriptioon of the vehicle
+
+    @DocumentReference // in the db, the id of each feature will be stored in the Vehcile document so it will be a refrence
+    private List<Feature> vehicleFeatures;
+
+    @DocumentReference
+    private List<Review> reviewsOfVehicle;
+
     public Vehicle(){}
 
-    private double calculateAverageRating() {
+   /* private double calculateAverageRating() {
         if (reviewsOfVehicle == null || reviewsOfVehicle.length == 0) {
             return 0.0; // Return 0 or some sensible default
         }
@@ -32,5 +43,5 @@ public class Vehicle {
             sum += review.getRating();
         }
         return (double) sum / reviewsOfVehicle.length; // Cast to double for a more accurate average
-    }
+    }*/
 }
